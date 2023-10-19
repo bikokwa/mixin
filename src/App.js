ff7b59b92e7130dc56ae2a4452f588e723c6b9a5
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-10-19 14:18:56 +0300

    mixin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const animalFun = {
+  walk: () => <p>Walking...</p>,
+  sleep: () => <p>Sleeping...</p>,
+};
+
+const dogFn = {
+  bark: () => <p>Barking...</p>,
+};
+
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Lion {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+Object.assign(dogFn, animalFun);
+Object.assign(Dog.prototype, dogFn);
+
+const tuffy = new Dog("Tuffy");
 
 function App() {
   return (
     <div className="App">
+      {tuffy.bark()}
+      {tuffy.walk()}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
